a236e3e1979a9379bcb8ce9910916c91806b74f0
shrifdarwish7 <shrifdarwish7@github.com>
2020-10-02 18:12:12 +0200

    initial commit

---
+++ TheBest-iOS-Driver/Scenes/Login+Register/Login/View/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  TheBest-iOS-Driver
+//
+//  Created by Sherif Darwish on 10/2/20.
+//  Copyright © 2020 Sherif Darwish. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
